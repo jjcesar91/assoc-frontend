3e3decfb2b0ee12359a0df5a96fd2bbdd2212722
jjcesar91 <jj_91@hotmail.it>
2026-02-03 23:40:06 +0000

    dockerizzazione

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Needed for Docker
     proxy: {
       '/auth': {
-        target: 'http://localhost:80',
+        target: process.env.VITE_API_TARGET || 'http://localhost:80',
         changeOrigin: true,
         secure: false,
       },
